9c01e57d7797af588791c6e265d3489d4628ad8b
forafekt <forafekt@gmail.com>
2020-08-21 14:40:06 +0200

    Custom prompts

--- djangular_cli/terminal/custom_prompts.py
+++ djangular_cli/terminal/custom_prompts.py
@@ -1,3 +1,8 @@
+"""
+Custom prompts
+"""
+
+
 def confirm(overwrite):
     """
     Confirm
@@ -12,6 +17,16 @@ prompt_overwrite = [
         'type': 'confirm',
         'name': 'overwrite',
         'message': 'Path already exists! Do you want to overwrite?',
+        'when': lambda overwrite: overwrite.get('overwrite', bool)
+    }
+]
+
+
+prompt_rename = [
+    {
+        'type': 'confirm',
+        'name': 'rename',
+        'message': 'Would you like to rename root directory?',
         'when': lambda overwrite: overwrite.get('overwrite', bool)
     }
 ]
